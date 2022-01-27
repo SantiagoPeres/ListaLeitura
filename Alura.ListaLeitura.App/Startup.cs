4bf0f41b5ee036cb0bb4a44111f753039c78fb6c
SantiagoPeres <santiagogperes@gmail.com>
2022-01-27 19:25:05 -0300

    Alura Web 02

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Rotemento);
+        }
+        public Task Roteamento(HttpContext context)
+        {
+            return context.Response.WriteAsync(context.Request.Path);
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+
+        }
+    }
+}
