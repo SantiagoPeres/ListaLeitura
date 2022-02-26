e19ca24891b9d3b57d78f20f1af14c463187d55d
SantiagoPeres <santiagogperes@gmail.com>
2022-02-26 18:35:52 -0300

    finalizando

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -7,7 +7,6 @@ namespace Alura.ListaLeitura.App
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRouting();
             services.AddMvc();
         }
 
